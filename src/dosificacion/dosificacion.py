755f774e0efad41ef34307b827df698f250055cc
Juan Canqui <naujjuan91@gmail.com>
2021-06-05 03:28:38 +0200

    dosificacion inicio

--- src/dosificacion/dosificacion.py
+++ src/dosificacion/dosificacion.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps({"dosificacion":"23456789fghjk","temp":event})
+        'body': json.dumps({"dosificacion":"1111111111111","temp":event})
     }
