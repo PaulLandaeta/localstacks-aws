c77f024a409394ad2d967758b09d83fe0be9fc42
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-07-10 12:01:19 -0400

    send email

--- maestria.sh
+++ maestria.sh
@@ -12,3 +12,49 @@ sudo docker run -m 8G --memory-reservation 2G --hostname=quickstart.cloudera --p
 
 .avsc
 
+avro-tools random --count 100 --schema-file example_avro.avsc example_avro.avro
+
+
+6  cat example_avro.avsc
+7  avro-tools --help
+8  avro-tools random
+9  avro-tools random --count 1 --schema-file example_avro.avsc
+10  avro-tools random --count 1 --schema-file example_avro.avsc -
+11  avro-tools random --count 100 --schema-file example_avro.avsc example_avro.avro
+12  ls
+13  ls -l
+14  cat example_avro.avro
+15  avro-tools --help
+16  avro-tools getschema
+17  avro-tools getschema example_avro.avro
+18  avro-tools getmeta example_avro.avro
+19  avro-tools tojson
+20  avro-tools tojson --pretty example_avro.avro
+21  avro-tools tojson --pretty example_avro.avro > example_avro.json
+
+40  ls -l
+41  ls -lh
+42  hadoop fs -ls /
+43  hadoop fs -ls /user/cloudera
+44  hadoop fs -mkdir /user/cloudera/2021_07_10
+45  hadoop fs -put example_avro_500.avro /user/cloudera/2021_07_10/
+46  hadoop fs -ls /user/cloudera
+47  less example_avro_500.avro
+48  hadoop fs -ls /user/cloudera/2021_07_10
+49  hadoop fs -get /user/cloudera/2021_07_10/loan_1000.csv
+50  ls -l
+51  head -n3 loan_1000.csv
+
+39  avro-tools random --count 500 --schema-file example_avro.avsc example_avro_500.avro
+40  ls -l
+41  ls -lh
+42  hadoop fs -ls /
+43  hadoop fs -ls /user/cloudera
+44  hadoop fs -mkdir /user/cloudera/2021_07_10
+45  hadoop fs -put example_avro_500.avro /user/cloudera/2021_07_10/
+46  hadoop fs -ls /user/cloudera
+47  less example_avro_500.avro
+48  hadoop fs -ls /user/cloudera/2021_07_10
+49  hadoop fs -get /user/cloudera/2021_07_10/loan_1000.csv
+50  ls -l
+51  head -n3 loan_1000.csv
--- src/mail/mail.py
+++ src/mail/mail.py
@@ -1,6 +1,19 @@
 import json
 import boto3
+import smtplib, ssl
 from botocore.exceptions import ClientError
+
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "paulwilkerlf3@gmail.com"  # Enter your address
+receiver_email = "naujjuan91@gmail.com"  # Enter receiver address
+password = "colegio123loco123"
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
 
 def lambda_handler(event, context): 
     """AWS Lambda Function entrypoint to create bill
@@ -19,14 +32,20 @@ def lambda_handler(event, context):
     boolean
 
     """
-    body = json.loads(event['body'])
+    # body = json.loads(event['body'])
+    print(event)
     queue_name = 'DemoStandardQueue'
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
 
     try:
         
         return {
             'statusCode': 200,
-            'body': json.dumps({"message": 'Factura Recibida a la cola', "event": body})
+            'body': json.dumps({"message": 'Factura Recibida a la cola'})
         }
     
     except ClientError as err:
